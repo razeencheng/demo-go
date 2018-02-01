6a85b967870a0ce0a807103fbde18cf5849c5871
razeencheng <razeen.cheng@gmail.com>
2018-02-01 16:38:36 +0800

    json-tag-demo

---
+++ json/tag/tag.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Nickname  string    `json:"nickname,omitempty"`
+	Sex       int       `json:"sex,string"`
+	Age       int       `json:"age,omitempty"`
+	AgeStr    int       `json:"age,omitempty,string"`
+	Merried   bool      `json:"merried,omitempty"`
+	Relation  *Relation `json:"relation"`
+}
+
+type Relation struct {
+	Mma *Person `json:"mma"`
+	Son *Person `json:"son,omitempty"`
+}
+
+func main() {
+	xiaoming := &Person{
+		FirstName: "xiaoming",
+		Nickname:  "",
+		AgeStr:    18,
+		Relation:  &Relation{},
+	}
+	buf, err := json.MarshalIndent(xiaoming, "", "")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(buf))
+}
