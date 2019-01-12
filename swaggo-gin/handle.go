4de999158c6b956c237d914b5ae13a1374df599e
Razeen Cheng <razeen.cheng@gmail.com>
2019-01-13 00:07:12 +0800

    feat(swaggo): 完成Swaggo Demo

--- swaggo-gin/handle.go
+++ swaggo-gin/handle.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
@@ -22,12 +24,37 @@ func HandleAuth(c *gin.Context) {
 }
 
 // HandleHello doc
+// @Summary 测试SayHello
+// @Description 向你说Hello
+// @Tags 测试
+// @Accept mpfd
+// @Produce json
+// @Param who query string true "人名"
+// @Success 200 {string} json "{"msg": "hello Razeen"}"
+// @Failure 400 {string} json "{"msg": "who are you"}"
+// @Router /hello [get]
 func HandleHello(c *gin.Context) {
 	who := c.Query("who")
+
+	if who == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"msg": "who are u?"})
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{"msg": "hello " + who})
 }
 
 // HandleLogin doc
+// @Summary 登陆
+// @Tags 登陆
+// @Description 登入
+// @Accept mpfd
+// @Produce json
+// @Param user formData string true "用户名" default(admin)
+// @Param password formData string true "密码"
+// @Success 200 {string} json "{"msg":"login success"}"
+// @Failure 400 {string} json "{"msg": "user or password error"}"
+// @Router /login [post]
 func HandleLogin(c *gin.Context) {
 	user := c.PostForm("user")
 	pwd := c.PostForm("password")
@@ -44,6 +71,14 @@ func HandleLogin(c *gin.Context) {
 }
 
 // HandleUpload doc
+// @Summary 上传文件
+// @Tags 文件处理
+// @Description 上传文件
+// @Accept mpfd
+// @Produce json
+// @Param file formData file true "文件"
+// @Success 200 {object} main.File
+// @Router /upload [post]
 func HandleUpload(c *gin.Context) {
 
 	fileHeader, err := c.FormFile("file")
@@ -67,19 +102,22 @@ func HandleUpload(c *gin.Context) {
 		}
 	}
 
-	f := &File{Name: fileHeader.Filename, Len: int(fileHeader.Size), Content: fileCon[:n]}
+	id++
+	f := &File{ID: id, Name: fileHeader.Filename, Len: int(fileHeader.Size), Content: fileCon[:n]}
 	files.Files = append(files.Files, f)
 	files.Len++
 	c.JSON(http.StatusOK, f)
 }
 
 var files = Files{Files: []*File{}}
+var id int
 
 // File doc
 type File struct {
+	ID      int    `json:"id"`
 	Name    string `json:"name"`
 	Len     int    `json:"len"`
-	Content []byte `json:"content"`
+	Content []byte `json:"-"`
 }
 
 // Files doc
@@ -89,6 +127,44 @@ type Files struct {
 }
 
 // HandleList doc
+// @Summary 查看文件列表
+// @Tags 文件处理
+// @Description 文件列表
+// @Accept mpfd
+// @Produce json
+// @Success 200 {object} main.Files
+// @Router /list [get]
 func HandleList(c *gin.Context) {
 	c.JSON(http.StatusOK, files)
+}
+
+// HandleGetFile doc
+// @Summary 获取某个文件
+// @Tags 文件处理
+// @Description 获取文件
+// @Accept mpfd
+// @Produce octet-stream
+// @Param id path integer true "文件ID"
+// @Success 200 {string} json ""
+// @Router /file/{id} [get]
+func HandleGetFile(c *gin.Context) {
+	fid, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"msg": err})
+		return
+	}
+
+	for _, f := range files.Files {
+		if f.ID == fid {
+			c.Writer.WriteHeader(http.StatusOK)
+			c.Header("Access-Control-Expose-Headers", "Content-Disposition")
+			c.Header("Content-Disposition", "attachment; "+f.Name)
+			c.Header("Content-Type", "application/octet-stream")
+			c.Header("Accept-Length", fmt.Sprintf("%d", len(f.Content)))
+			c.Writer.Write(f.Content)
+			return
+		}
+	}
+
+	c.JSON(http.StatusBadRequest, gin.H{"msg": "no avail file"})
 }
--- swaggo-gin/main.go
+++ swaggo-gin/main.go
@@ -1,21 +1,46 @@
 package main
 
 import (
+	"github.com/gin-gonic/contrib/sessions"
 	"github.com/gin-gonic/gin"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
+
+	_ "github.com/razeencheng/demo-go/swaggo-gin/docs"
 )
+
+// @title Swagger Example API
+// @version 1.0
+// @description This is a sample server celler server.
+// @termsOfService https://razeen.me
+
+// @contact.name Razeen
+// @contact.url https://razeen.me
+// @contact.email me@razeen.me
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host 127.0.0.1:8080
+// @BasePath /api/v1
 
 func main() {
 
 	r := gin.Default()
+	store := sessions.NewCookieStore([]byte("secret"))
+	r.Use(sessions.Sessions("mysession", store))
 
-	v1 := r.Group("api/v1")
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	v1 := r.Group("/api/v1")
 	{
 		v1.GET("/hello", HandleHello)
 		v1.POST("/login", HandleLogin)
-		v1Auth := r.Use(HandleAuth)
+		v1Auth := v1.Use(HandleAuth)
 		{
 			v1Auth.POST("/upload", HandleUpload)
 			v1Auth.GET("/list", HandleList)
+			v1Auth.GET("/file/:id", HandleGetFile)
 		}
 	}
 
